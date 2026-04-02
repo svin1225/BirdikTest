4b96a313ece3bcfe62ec14226305a986b3cb61ee
svin1225 <ivangorodnov013@gmail.com>
2026-04-02 20:54:25 +0700

    cameraCollision

--- Assets/PovorotX.cs
+++ Assets/PovorotX.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class PovorotX : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform playerBody;
+    public Transform cameraTransform;
+    public float cameraDistance = 2f;
+    public float minDistance = 0.1f;
+    public LayerMask obstacleLayer;
+    
     void Start()
     {
-        
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
+        if (playerBody == null)
+            playerBody = transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector3 cameraOffset = new Vector3(0, 2, -cameraDistance);
+        Vector3 desiredCameraPos = playerBody.position + playerBody.rotation * cameraOffset;
         
+        RaycastHit hit;
+        Vector3 direction = desiredCameraPos - (playerBody.position + Vector3.up * 1);
+        
+        if (Physics.Raycast(playerBody.position + Vector3.up * 1, direction, out hit, cameraDistance, obstacleLayer))
+        {
+            float distance = Mathf.Max(hit.distance * 0.9f, minDistance);
+            cameraTransform.position = (playerBody.position + Vector3.up * 1) + direction.normalized * distance;
+        }
+        else
+        {
+            cameraTransform.position = desiredCameraPos;
+        }
+        
+        cameraTransform.LookAt(playerBody.position + Vector3.up * 1);
     }
 }
